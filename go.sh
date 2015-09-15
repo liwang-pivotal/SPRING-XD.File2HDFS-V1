01d721cb7c14489ab44a0afad14dad5815197e0a
Li Wang <liwang@pivotal.io>
2015-09-15 19:13:18 -0400

    read resources from a folder

--- go.sh
+++ go.sh
@@ -1,3 +1,7 @@
 gradle clean build -x test
+
 cp build/libs/File2HDFS-FSH-1.1.0.RELEASE.jar /Users/lwang/Documents/spring-xd-1.2.1.RELEASE/xd/custom-modules/job/F2H.jar
 cp build/libs/File2HDFS-FSH-1.1.0.RELEASE.jar /Users/lwang/Documents/spring-xd-1.2.1.RELEASE/xd/custom-modules/job/F2H.jar.md5
+
+scp build/libs/File2HDFS-FSH-1.1.0.RELEASE.jar admin:/lwang/custom-modules/job/F2H.jar
+scp build/libs/File2HDFS-FSH-1.1.0.RELEASE.jar admin:/lwang/custom-modules/job/F2H.jar.md5
